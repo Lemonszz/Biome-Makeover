f6a447d1ad150216055388fd06975fa92c75aeb4
Sam <lemonverse@gmail.com>
2022-05-20 16:22:41 +1200

    Fix #104

--- common/src/main/java/party/lemons/biomemakeover/entity/CowboyEntity.java
+++ common/src/main/java/party/lemons/biomemakeover/entity/CowboyEntity.java
@@ -4,12 +4,21 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.monster.Pillager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.raid.Raid;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.entity.BannerPattern;
@@ -38,6 +47,47 @@ public class CowboyEntity extends Pillager {
             Entity v = getVehicle();
             v.setYRot(getYRot());
         }
+    }
+
+    @Override
+    public void die(DamageSource damageSource)
+    {
+        //Doesn't trigger bad omen without this since it checks if the banner == raid banner
+        if (this.level instanceof ServerLevel) {
+            Entity entity = damageSource.getEntity();
+            if (this.isPatrolLeader() && raid == null && ((ServerLevel)this.level).getRaidAt(this.blockPosition()) == null) {
+                ItemStack itemStack = this.getItemBySlot(EquipmentSlot.HEAD);
+                Player player = null;
+                Entity entity2 = entity;
+                if (entity2 instanceof Player) {
+                    player = (Player)entity2;
+                }
+                else if (entity2 instanceof Wolf)
+                {
+                    Wolf wolf = (Wolf)entity2;
+                    LivingEntity livingEntity = wolf.getOwner();
+                    if (wolf.isTame() && livingEntity instanceof Player) {
+                        player = (Player)livingEntity;
+                    }
+                }
+                if (!itemStack.isEmpty() && ItemStack.matches(itemStack, getOminousBanner()) && player != null) {
+                    MobEffectInstance mobEffectInstance = player.getEffect(MobEffects.BAD_OMEN);
+                    int i = 1;
+                    if (mobEffectInstance != null) {
+                        i += mobEffectInstance.getAmplifier();
+                        player.removeEffectNoUpdate(MobEffects.BAD_OMEN);
+                    } else {
+                        --i;
+                    }
+                    i = Mth.clamp(i, 0, 4);
+                    MobEffectInstance mobEffectInstance2 = new MobEffectInstance(MobEffects.BAD_OMEN, 120000, i, false, false, true);
+                    if (!this.level.getGameRules().getBoolean(GameRules.RULE_DISABLE_RAIDS)) {
+                        player.addEffect(mobEffectInstance2);
+                    }
+                }
+            }
+        }
+        super.die(damageSource);
     }
 
     @Nullable
