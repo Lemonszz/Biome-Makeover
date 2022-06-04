930fdb5a4874756cb07e74cfb7bf8e271ec110ff
Sam <lemonverse@gmail.com>
2022-06-04 19:04:46 +1200

    Port to new fabric loot table injection

--- fabric/src/main/java/party/lemons/biomemakeover/fabric/BMFabric.java
+++ fabric/src/main/java/party/lemons/biomemakeover/fabric/BMFabric.java
@@ -5,20 +5,24 @@ import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.*;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableSource;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.minecraft.core.Holder;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BiomeTags;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTables;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemKilledByPlayerCondition;
@@ -56,15 +60,20 @@ public class BMFabric implements ModInitializer
 
     private void injectLootTables()
     {
-        LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
-            for(BMLootTableInjection.InjectedItem item : BMLootTableInjection.getInsertedEntries())
+        LootTableEvents.MODIFY.register(Event.DEFAULT_PHASE, new LootTableEvents.Modify()
+        {
+            @Override
+            public void modifyLootTable(ResourceManager resourceManager, LootTables lootManager, ResourceLocation id, LootTable.Builder tableBuilder, LootTableSource source)
             {
-                if(id.equals(item.table()))
+                for(BMLootTableInjection.InjectedItem item : BMLootTableInjection.getInsertedEntries())
                 {
-                    supplier.withPool(LootPool.lootPool()
-                            .setRolls(item.rolls())
-                            .add(LootItem.lootTableItem(item.itemLike())
-                                    .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0f, 2.0f)))).when(LootItemKilledByPlayerCondition.killedByPlayer()).build());
+                    if(id.equals(item.table()))
+                    {
+                        tableBuilder.withPool(
+                                LootPool.lootPool().setRolls(item.rolls())
+                                        .add(LootItem.lootTableItem(item.itemLike())
+                                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0f, 2.0f)))).when(LootItemKilledByPlayerCondition.killedByPlayer()));
+                    }
                 }
             }
         });
