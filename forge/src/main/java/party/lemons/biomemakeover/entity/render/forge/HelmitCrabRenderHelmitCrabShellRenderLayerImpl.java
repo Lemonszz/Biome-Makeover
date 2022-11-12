1ccf4b447632f1ded06264a075528642cdba8e89
Sam <lemonverse@gmail.com>
2022-11-12 18:49:08 +1300

    Fix helmit crab helmets not rendering

--- forge/src/main/java/party/lemons/biomemakeover/entity/render/forge/HelmitCrabRenderHelmitCrabShellRenderLayerImpl.java
+++ forge/src/main/java/party/lemons/biomemakeover/entity/render/forge/HelmitCrabRenderHelmitCrabShellRenderLayerImpl.java
@@ -5,17 +5,25 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.ForgeHooksClient;
 
+import java.util.Locale;
 import java.util.Map;
 
 public class HelmitCrabRenderHelmitCrabShellRenderLayerImpl
 {
 	public static ResourceLocation getArmorTexture(Map<String, ResourceLocation> cache, ItemStack stack, Entity entity, EquipmentSlot slot, String type)
 	{
-		String texture = stack.getItem().getArmorTexture(stack, entity, slot, type);
-		if(texture != null)
+		String path = null;
+		if(stack.getItem() instanceof ArmorItem armorItem)
 		{
-			return cache.computeIfAbsent(texture, ResourceLocation::new);
+			path = "textures/models/armor/" + armorItem.getMaterial().getName() + "_layer_1" + (type == null ? "" : "_" + type) + ".png";
+		}
+		path = ForgeHooksClient.getArmorTexture(entity, stack, path, slot, type);
+		if(path != null)
+		{
+			if(ResourceLocation.isValidResourceLocation(path))
+				return cache.computeIfAbsent(path, ResourceLocation::new);
 		}
 		return null;
 	}
