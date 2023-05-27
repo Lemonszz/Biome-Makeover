92ad190a588ac47b469aa9d720ac6a16236cdb9c
Sam <lemonverse@gmail.com>
2023-05-27 13:59:57 +1200

    relocate sign and armor textures

--- common/src/main/java/party/lemons/biomemakeover/init/BMBoats.java
+++ common/src/main/java/party/lemons/biomemakeover/init/BMBoats.java
@@ -2,14 +2,16 @@ package party.lemons.biomemakeover.init;
 
 import party.lemons.biomemakeover.BiomeMakeover;
 import party.lemons.taniwha.block.WoodBlockFactory;
+import party.lemons.taniwha.entity.boat.BoatShape;
 import party.lemons.taniwha.entity.boat.BoatType;
+import party.lemons.taniwha.entity.boat.BoatTypes;
 
 public class BMBoats
 {
-	public static final BoatType BLIGHTED_BALSA = new BoatType(BiomeMakeover.ID("blighted_balsa"), ()-> BMBlocks.BLIGHTED_BALSA_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()-> BMBlocks.BLIGHTED_BALSA_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
-	public static final BoatType WILLOW = new BoatType(BiomeMakeover.ID("willow"), ()->BMBlocks.WILLOW_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.WILLOW_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
-	public static final BoatType SWAMP_CYPRESS = new BoatType(BiomeMakeover.ID("swamp_cypress"), ()->BMBlocks.SWAMP_CYPRESS_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.SWAMP_CYPRESS_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
-	public static final BoatType ANCIENT_OAK = new BoatType(BiomeMakeover.ID("ancient_oak"), ()->BMBlocks.ANCIENT_OAK_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.ANCIENT_OAK_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
+	public static final BoatType BLIGHTED_BALSA = new BoatType(BiomeMakeover.ID("blighted_balsa"), BoatTypes.REGULAR_SHAPE, ()-> BMBlocks.BLIGHTED_BALSA_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()-> BMBlocks.BLIGHTED_BALSA_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
+	public static final BoatType WILLOW = new BoatType(BiomeMakeover.ID("willow"),  BoatTypes.REGULAR_SHAPE, ()->BMBlocks.WILLOW_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.WILLOW_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
+	public static final BoatType SWAMP_CYPRESS = new BoatType(BiomeMakeover.ID("swamp_cypress"),  BoatTypes.REGULAR_SHAPE, ()->BMBlocks.SWAMP_CYPRESS_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.SWAMP_CYPRESS_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
+	public static final BoatType ANCIENT_OAK = new BoatType(BiomeMakeover.ID("ancient_oak"),  BoatTypes.REGULAR_SHAPE, ()->BMBlocks.ANCIENT_OAK_WOOD_INFO.getItem(WoodBlockFactory.Type.BOAT).get(), ()->BMBlocks.ANCIENT_OAK_WOOD_INFO.getItem(WoodBlockFactory.Type.CHEST_BOAT).get());
 
 
 	public static void init()
--- common/src/main/java/party/lemons/biomemakeover/init/BMItems.java
+++ common/src/main/java/party/lemons/biomemakeover/init/BMItems.java
@@ -242,7 +242,7 @@ public class BMItems
         @Override
         public String getName()
         {
-            return "cladded";
+            return Constants.MOD_ID + ":cladded";
         }
 
         @Override
