1e0e0237ef67b5e8c5d69b41b6c31623c3deb053
Sam <lemonverse@gmail.com>
2023-10-14 17:56:45 +1300

    camel improvements

--- common/src/main/java/party/lemons/biomemakeover/entity/camel/EquipmentCamelEntity.java
+++ common/src/main/java/party/lemons/biomemakeover/entity/camel/EquipmentCamelEntity.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.animal.camel.Camel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
--- common/src/main/java/party/lemons/biomemakeover/entity/render/ChestCamelRender.java
+++ common/src/main/java/party/lemons/biomemakeover/entity/render/ChestCamelRender.java
@@ -2,6 +2,7 @@ package party.lemons.biomemakeover.entity.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.CamelModel;
 import net.minecraft.client.model.EntityModel;
@@ -11,12 +12,19 @@ import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.CamelRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.animal.camel.Camel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import party.lemons.biomemakeover.BiomeMakeover;
 
@@ -27,79 +35,37 @@ public class ChestCamelRender extends CamelRenderer {
         addLayer(new CamelChestLayer(context, this));
     }
 
-    private class CamelChestLayer extends RenderLayer<Camel, CamelModel<Camel>>
+    private static class CamelChestLayer extends RenderLayer<Camel, CamelModel<Camel>>
     {
-        private static final ResourceLocation TEXTURE = BiomeMakeover.ID("textures/entity/camel/camel_chest.png");
+        private final BlockState STATE = Blocks.CHEST.defaultBlockState();
+        private final ChestBlockEntity chestDummy;
+        private float openness = 0.0F;
 
-        private final ModelPart model;
         public CamelChestLayer(EntityRendererProvider.Context context, RenderLayerParent<Camel, CamelModel<Camel>> renderLayerParent) {
             super(renderLayerParent);
 
-            model = context.bakeLayer(CamelChestModel.LAYER_LOCATION);
+            chestDummy = new ChestBlockEntity(BlockPos.ZERO, STATE){
+                @Override
+                public float getOpenNess(float f) {
+                    return openness;
+                }
+            };
         }
 
         @Override
-        public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int i, Camel entity, float f, float g, float h, float j, float k, float l)
-        {
-            model.getChild("root").setRotation(getParentModel().root().xRot/2.5F, getParentModel().root().yRot, getParentModel().root().zRot/2.5F);
-            model.getChild("root").getChild("body").copyFrom(getParentModel().root().getChild("body"));
-            int p = getOverlayCoords(entity, 0);
-            boolean visible = isBodyVisible(entity);
-            boolean isInvis = !visible && !entity.isInvisibleTo(Minecraft.getInstance().player);
-            boolean outline = Minecraft.getInstance().shouldEntityAppearGlowing(entity);
+        public void render(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, Camel entity, float f, float g, float h, float delta, float k, float l) {
             poseStack.pushPose();
-            poseStack.translate(0, -1.5, 0);
-            model.render(poseStack, multiBufferSource.getBuffer(this.getRenderType(visible, isInvis, outline)), i, p, 1.0F, 1.0F, 1.0F, 1.0F);
+            getParentModel().root().translateAndRotate(poseStack);
+            getParentModel().root().getChild("body").translateAndRotate(poseStack);
+            poseStack.scale(0.85F, 0.85F, 0.85F);
+            poseStack.translate(0.5, -0.88F, -0.4F);
+            poseStack.mulPose(Axis.ZP.rotationDegrees(180.0f));
+
+            openness = Mth.abs(getParentModel().root().zRot) / 2F;
+            Minecraft.getInstance().getBlockEntityRenderDispatcher().getRenderer(chestDummy).render(chestDummy, delta, poseStack, multiBufferSource, light, OverlayTexture.NO_OVERLAY);
 
             poseStack.popPose();
 
-        }
-
-        @Nullable
-        protected RenderType getRenderType(boolean bl, boolean bl2, boolean bl3) {
-            ResourceLocation resourceLocation = TEXTURE;
-            if (bl2) {
-                return RenderType.itemEntityTranslucentCull(resourceLocation);
-            } else if (bl) {
-                return RenderType.entityCutoutNoCull(resourceLocation);
-            } else {
-                return bl3 ? RenderType.outline(resourceLocation) : null;
-            }
-        }
-    }
-
-    public static class CamelChestModel extends EntityModel<Camel> {
-        public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(BiomeMakeover.ID("camel_chest"), "main");
-        private final ModelPart root;
-
-        public CamelChestModel(ModelPart root) {
-            this.root = root.getChild("root");
-        }
-
-        public static LayerDefinition createBodyLayer() {
-            MeshDefinition meshdefinition = new MeshDefinition();
-            PartDefinition partdefinition = meshdefinition.getRoot();
-
-            PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
-
-            PartDefinition body = root.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.5F, -20.0F, 9.5F));
-
-            PartDefinition Chest = body.addOrReplaceChild("Chest", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -41.0F, 5.0F, 12.0F, 9.0F, 12.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.5F, 20.0F, -9.5F));
-
-            PartDefinition Lid = Chest.addOrReplaceChild("Lid", CubeListBuilder.create().texOffs(0, 21).addBox(-6.0F, -34.5F, 10.5F, 12.0F, 3.0F, 12.0F, new CubeDeformation(0.0F))
-                    .texOffs(0, 0).addBox(-1.0F, -33.0F, 22.5F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -9.5F, -5.5F));
-
-            return LayerDefinition.create(meshdefinition, 128, 128);
-        }
-
-        @Override
-        public void setupAnim(Camel entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
-        }
-
-        @Override
-        public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-            root.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
         }
     }
 }
