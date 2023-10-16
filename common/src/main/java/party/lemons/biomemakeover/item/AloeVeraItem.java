0d29a9b0bfff4d76a5c9dd7dde5357c0003c8fe3
Sam <lemonverse@gmail.com>
2023-10-17 08:22:04 +1300

    Aloe applying

--- common/src/main/java/party/lemons/biomemakeover/item/AloeVeraItem.java
+++ common/src/main/java/party/lemons/biomemakeover/item/AloeVeraItem.java
@@ -1,5 +1,6 @@
 package party.lemons.biomemakeover.item;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
@@ -10,9 +11,9 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 import party.lemons.biomemakeover.util.EntityUtil;
 import party.lemons.taniwha.item.animation.CustomUseAnimationItem;
-import party.lemons.taniwha.util.ItemUtil;
 
 public class AloeVeraItem extends ItemNameBlockItem implements CustomUseAnimationItem {
     public AloeVeraItem(Block block, Properties properties) {
@@ -25,19 +26,45 @@ public class AloeVeraItem extends ItemNameBlockItem implements CustomUseAnimatio
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
-        player.startUsingItem(interactionHand);
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand)
+    {
+        ItemStack stack = player.getItemInHand(interactionHand);
 
-        return new InteractionResultHolder<>(InteractionResult.PASS, player.getItemInHand(interactionHand));
+        if(!player.isUsingItem()) {
+            player.startUsingItem(interactionHand);
+            return new InteractionResultHolder<>(InteractionResult.PASS, stack);
+        }
+        return new InteractionResultHolder<>(InteractionResult.PASS, stack);
+    }
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack itemStack, Player player, LivingEntity livingEntity, InteractionHand interactionHand) {
+        if(!player.isUsingItem()) {
+            applyAloe(livingEntity);
+            player.getCooldowns().addCooldown(this, 30);
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.PASS;
     }
 
     @Override
-    public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
-        if (livingEntity instanceof Player && !((Player)livingEntity).getAbilities().instabuild) {
-            itemStack.shrink(1);
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity user) {
+        if (user instanceof Player player && !player.getAbilities().instabuild) {
+            stack.shrink(1);
+            player.getCooldowns().addCooldown(this, 30);
         }
+        applyAloe(user);
 
-        return itemStack;
+        return stack;
+    }
+
+    public void applyAloe(LivingEntity user)
+    {
+        if(user.level().isClientSide())
+        {
+            user.clearFire();
+            user.heal(2F);
+            //TODO: particles/sound
+        }
     }
 
     @Override
@@ -47,6 +74,6 @@ public class AloeVeraItem extends ItemNameBlockItem implements CustomUseAnimatio
 
     @Override
     public void triggerItemUseEffects(LivingEntity livingEntity, ItemStack itemStack, int i) {
-        EntityUtil.spawnItemParticles(livingEntity, itemStack, 1);
+        EntityUtil.spawnItemParticles(livingEntity, itemStack, 2);
     }
 }
